7dbfa4f9866cbb53b50d9b9a4fecc26249e2368e
Hunter Werlla <werlla@amazon.com>
2021-02-09 10:10:30 -0800

    Add command line argument parsing

---
+++ src/cli/mod.rs
@@ -0,0 +1,63 @@
+use clap::{App, Arg, ArgMatches};
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+static LANGUAGES: [&str; 2] = ["java", "javascript"];
+
+pub enum SdkVersion {
+    Latest,
+    Number { value: i32 },
+}
+
+pub struct ParsedArguments {
+    pub version: SdkVersion,
+    pub language: String,
+    pub aws_cli_input: Vec<String>,
+}
+
+pub fn parse_command_line() -> ParsedArguments {
+    let matches = App::new(NAME)
+        .version(VERSION)
+        .arg(
+            Arg::new("language")
+                .short('l')
+                .long("language")
+                .possible_values(&LANGUAGES)
+                .required(true),
+        )
+        .arg(
+            Arg::new("sdk version")
+                .short('s')
+                .long("sdk-version")
+                .default_value("latest")
+                .about("Which SDK version to use"),
+        )
+        .arg(Arg::new("AWS cli command").multiple(true).last(true))
+        .get_matches();
+    let language = match matches.value_of("language") {
+        Some(language) => language,
+        None => panic!("Somehow got no language!"),
+    };
+    let sdk_version = match matches.value_of("sdk version") {
+        None => SdkVersion::Latest,
+        Some(version) => {
+            if version == "latest" {
+                SdkVersion::Latest
+            } else {
+                match version.parse::<i32>() {
+                    Ok(num) => SdkVersion::Number { value: num },
+                    Err(_) => panic!("Sdk version must be an integer or latest"),
+                }
+            }
+        }
+    };
+    let aws_cli_input = match matches.values_of("AWS cli command") {
+        Some(values) => values.map(|f| f.to_string()).collect::<Vec<String>>(),
+        None => panic!("Need aws cli input!"),
+    };
+    ParsedArguments {
+        version: sdk_version,
+        language: language.to_string(),
+        aws_cli_input,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // aws s3api get-bucket-acl --bucket jetbrainss3testbucketa
 
+mod cli;
+mod generator;
 mod parser;
 
+use crate::cli::parse_command_line;
 use crate::parser::load_and_parse_service;
 use crate::parser::parser::Service2;
 use serde::Deserialize;
@@ -10,6 +13,11 @@ use std::collections::HashMap;
 use std::fs::File;
 
 fn main() -> Result<(), std::io::Error> {
+    let arguments = parse_command_line();
+    arguments
+        .aws_cli_input
+        .iter()
+        .for_each(|f| println!("{}", f));
     let parsed = load_and_parse_service("resources/ecr")?;
     /*
     parsed.shapes.keys().for_each(|item| {
